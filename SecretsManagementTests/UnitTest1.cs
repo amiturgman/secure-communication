54854c71e5cc9672855c90a1401eedfeb1c42086
Elad Iwanir <eladiw@microsoft.com>
2017-12-03 14:39:31 +0200

    adding a few tests to the secretsMgmnt

--- SecretsManagementTests/UnitTest1.cs
+++ SecretsManagementTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using SecuredCommunication;
 using Xunit;
 
 namespace SecretsManagementTests
@@ -6,9 +7,41 @@ namespace SecretsManagementTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Sanity_VerifyCanBeCreated()
         {
+            var kvInfo = new KeyVaultInfo("http://dummyKvUri");
+            var secMgmnt = new SecretsManagement(kvInfo);
+        }
 
+        [Fact]
+        public async void Sanity_Encryption()
+        {
+            var kvUri = "http://dummyKvUri";
+            var rawData = "Some data !!!";
+            var kvInfo = new KeyVaultInfo(kvUri);
+            var secMgmnt = new SecretsManagement(kvInfo);
+            var encryptedData = await secMgmnt.Encrypt(kvUri, "encKey", rawData);
+
+            Assert.NotEqual(encryptedData, rawData);
+        }
+
+        [Fact]
+        public async void Sanity_Decryption()
+        {
+            var kvUri = "http://dummyKvUri";
+            var rawData = "Some data !!!";
+            var kvInfo = new KeyVaultInfo(kvUri);
+            var secMgmnt = new SecretsManagement(kvInfo);
+
+            // Encrypt
+            var encryptedData = await secMgmnt.Encrypt(kvUri, "encKey", rawData);
+
+            // Decrypt
+            var decryptedData = await secMgmnt.Decrypt(kvUri, "encKey", encryptedData);
+
+            // Verify the process ended succesfully and the data is plain text
+            Assert.NotEqual(encryptedData, rawData);
+            Assert.Equal(decryptedData, rawData);
         }
     }
 }
