d02596977253c7cd86ac9bbe957a3d4ee0133ce5
Ilana Kantorov <ilanak@microsoft.com>
2017-12-20 10:37:42 +0200

    Update message class

--- Contracts/Message.cs
+++ Contracts/Message.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 
 namespace Contracts
 {
@@ -12,12 +13,16 @@ namespace Contracts
         public byte[] Data { get; }
         public byte[] Signature { get; }
 
+        /// <summary>
+        /// Ctor for message that is passed in the communication pipeline
+        /// </summary>
+        /// <param name="isEncrypted">A flag indicates whether the message is Encrypted</param>
+        /// <param name="data">A byte array of the data to send</param>
+        /// <param name="signature">The siganture on the data</param>
         public Message(bool isEncrypted, byte[] data, byte[] signature)
         {
-            if (data == null || signature == null)
-            {
-                throw new ArgumentException("invalid input data");
-            }
+            Contract.Requires<ArgumentNullException>(data != null, "Parameter cannot be null.");
+            Contract.Requires<ArgumentNullException>(signature != null, "Parameter cannot be null.");
 
             Encrypted = isEncrypted;
             Data = data;
---
+++ SecuredCommTests/MessageTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Xml.Xsl;
+using Contracts;
+using Xunit;
+
+namespace UnitTests
+{
+    public class MessageTests
+    {
+        [Fact]
+        public void Test_Message_Created_As_Expected()
+        {
+            //var isEncrypted = true;
+            //var 
+            //var msg = new Message()
+        }
+    }
+}
