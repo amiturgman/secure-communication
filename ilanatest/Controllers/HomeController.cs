6d6c18344ad8740869fad547d6268e86b5976970
Ilana Kantorov <ilanak@microsoft.com>
2018-01-11 14:04:02 +0200

    web app working

---
+++ ilanatest/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using ilanatest.Models;
+using SecuredCommunication;
+
+namespace ilanatest.Controllers
+{
+    public class HomeController : Controller
+    {
+        AzureQueueImpl m_comm;
+
+        public HomeController(IQueueManager _comm)
+        {
+            m_comm = (AzureQueueImpl)_comm;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ilanatest/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SecuredCommunication;
+
+namespace ilanatest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            
+        }
+
+        public IConfiguration Configuration { get; }
+        public KeyVault KV;
+        public AzureQueueImpl securedComm;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSingleton<IQueueManager, AzureQueueImpl>((serviceProvider) =>
+            {
+                KV = new KeyVault(Configuration["AzureKeyVaultUri"],
+                    Configuration["applicationId"], Configuration["applicationSecret"]);
+                var encryptionKeyName = Configuration["EncryptionKeyName"];
+                var decryptionKeyName = Configuration["DecryptionKeyName"];
+                var signKeyName = Configuration["SignKeyName"];
+                var verifyKeyName = Configuration["VerifyKeyName"];
+
+                var secretsMgmnt = new KeyVaultSecretManager(encryptionKeyName, decryptionKeyName, signKeyName, verifyKeyName, KV, KV);
+                secretsMgmnt.Initialize().Wait();
+                //var securedComm = new RabbitMQBusImpl(config["rabbitMqUri"], secretsMgmnt, true, "securedCommExchange");
+                var queueClient = new CloudQueueClientWrapper(Configuration["AzureStorageConnectionString"]);
+                securedComm = new AzureQueueImpl("ilanatest", queueClient, secretsMgmnt, true);
+                securedComm.Initialize().Wait();
+                securedComm.DequeueAsync(msg =>
+                {
+                    Console.WriteLine(msg);
+                }, TimeSpan.FromSeconds(1));
+                return securedComm;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
